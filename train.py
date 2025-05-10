e95d333efe2f104ac9406a5d2730d9e5484507b4
PJH6029 <top231902@naver.com>
2025-05-10 21:03:11 +0900

    Fix typo

--- train.py
+++ train.py
@@ -2,12 +2,12 @@ import argparse
 import datetime
 from arc import ARCSolver, ARCDataset
 
-WORDKSPACE = '/home/top321902/code/intro_dl/term_project'
+WORKSPACE = '/home/top321902/code/intro_dl/term_project'
 
 def main():
     parser = argparse.ArgumentParser(description='Train ARCSolver with ARC dataset')
     parser.add_argument('--token', type=str, default=None, help='HuggingFace token')
-    parser.add_argument('--dataset', type=str, default=f"{WORDKSPACE}/dataset", help='Dataset path')
+    parser.add_argument('--dataset', type=str, default=f"{WORKSPACE}/dataset", help='Dataset path')
     args = parser.parse_args()
     
     ##############
